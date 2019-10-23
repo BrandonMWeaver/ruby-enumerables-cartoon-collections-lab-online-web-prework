9ab6ad71b0fe45a7cb87ca2590c2ebbc7f7d83cf
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 23:13:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ def summon_captain_planet(planeteer_calls)
   return temp
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
 end
 
 def find_the_cheese# code an argument here
